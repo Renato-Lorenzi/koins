232975a5ea4369eed4e82907fc7bb7477ecf9a8f
Kelvin Raffal Stinghen <kelvin.stinghen@gmail.com>
2013-09-23 14:27:24 -0300

    rails new koins! :train::moneybag:

--- README.md
+++ README.md
@@ -6,3 +6,28 @@
 **Koins** is a clean and free finance manager!
 
 Probably you are asking yourself: another one? But this is totally different, I promisse you. Why? I'll explain for you soon...
+
+## rails readme
+
+This README would normally document whatever steps are necessary to get the
+application up and running.
+
+Things you may want to cover:
+
+* Ruby version
+
+* System dependencies
+
+* Configuration
+
+* Database creation
+
+* Database initialization
+
+* How to run the test suite
+
+* Services (job queues, cache servers, search engines, etc.)
+
+* Deployment instructions
+
+* ...
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Koins::Application.config.secret_key_base = '17f2973fa256698818fec32869fb5e6f0786dda876a7d600306bd79960391c704aa4b3fe19b94e2d934bbe283f5b8b516f294d2d25b71c3184c833334c4b3510'
